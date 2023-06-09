8feb32e92c10596c3b9823af571cde9d95262ad8
shubhamtade <shubham.tade@gmail.com>
2023-06-09 08:32:38 -0700

    final work

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { SlMenu } from "react-icons/sl";
+import { FaUserCircle } from "react-icons/fa";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
+const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+  };
+
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+    setMobileMenu(false);
+  };
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo" onClick={() => navigate("/")}>
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            TV Shows
+          </li>
+          <li className="menuItem">
+            <FaUserCircle size={30} />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems">
+          <FaUserCircle size={20} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.scss";
+
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+
+const Home = () => {
+    return (
+        <div className="homePage">
+            <HeroBanner />
+            <Trending />
+            <Popular />
+        </div>
+    );
+};
+
+export default Home;
